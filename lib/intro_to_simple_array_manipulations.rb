e7b565dbbd46ccfcdee05a199a158c0ee5bddd65
Max Petersen <mglxp5@gmail.com>
2018-09-02 21:29:44 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,8 +38,10 @@ def using_flatten(arr)
   arr.flatten
 end
 
-def using_delete
+def using_delete(arr, str)
+  arr.delete(str)
 end
 
-def using_delete_at
+def using_delete_at(arr, int)
+  arr.delete()
 end
